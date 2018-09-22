b8ba8402b44701703d27ac1aab1429e2cfc97a57
Anwesh43 <anweshthecool0@gmail.com>
2018-09-22 18:23:28 +0530

    adding QCNode class

--- LinkedQuarterCircleStage.ts
+++ LinkedQuarterCircleStage.ts
@@ -71,3 +71,67 @@ class Animator {
         }
     }
 }
+
+class QCNode {
+    prev : QCNode
+    next : QCNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new QCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    getNext(dir : number, cb : Function) : QCNode {
+        var curr : QCNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = h / (nodes + 1)
+        const r : number = gap / 3
+        const deg : number = (2 * Math.PI) / k
+        const factor : number = 1 / k
+        context.save()
+        context.translate(w/2, gap * this.i + gap)
+        for(var j = 0; j <  k ; j++) {
+            const sc : number = Math.min(factor, Math.max(0, this.state.scale - factor * j)) * k
+            context.save()
+            context.rotate(deg * j)
+            context.beginPath()
+            for (var t = 0; t <= 90 * sc; t++) {
+                const xa : number = r * Math.cos(t * Math.PI/180)
+                const ya : number = r * Math.sin(t * Math.PI/180)
+                if (t == 0) {
+                    context.moveTo(xa, ya)
+                } else {
+                    context.lineTo(xa, ya)
+                }
+                context.fill()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+}
