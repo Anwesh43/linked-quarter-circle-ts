cfe957393f80fae1de4f354d21cd5299f4c95f28
Anwesh43 <anweshthecool0@gmail.com>
2018-09-22 18:27:30 +0530

    adding LinkedQuarterCircle class

--- LinkedQuarterCircleStage.ts
+++ LinkedQuarterCircleStage.ts
@@ -135,3 +135,25 @@ class QCNode {
         this.state.startUpdating(cb)
     }
 }
+
+class LinkedQuarterCircle {
+    curr : QCNode = new QCNode(0)
+    dir : number = 1
+
+    draw(ctx : CanvasRenderingContext2D) {
+        this.curr.draw(ctx)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
